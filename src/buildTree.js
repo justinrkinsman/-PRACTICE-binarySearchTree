2afdf19e0459c4ad789607b83ee5d20e0c5eba00
Justin <justinrkinsman@gmail.com>
2022-08-15 10:06:30 -0300

    Create test for removing duplicate values from array

--- src/buildTree.js
+++ src/buildTree.js
@@ -2,7 +2,7 @@ let array = [1, 2, 3, 4, 5, 6, 7]
 
 let start = 0
 let end = array.length - 1
-let mid = (start+end)/2
+let mid = Math.floor((start+end)/2)
 
 class Node {
     constructor(data){
@@ -20,8 +20,13 @@ class Tree {
 
 function buildTree(arr) {
     arr.sort((a, b) => a - b)
+    let start = 0
+    let end = arr.length - 1
+    let mid = Math.floor((start+end)/2)
     let root = arr[mid]
     return root
 }
+
+console.log(buildTree)
 
 module.exports = buildTree
--- src/buildTree.test.js
+++ src/buildTree.test.js
@@ -1,14 +1,34 @@
 const buildTree = require("./buildTree.js");
 
 test('Initial test', () => {
-    let array = [5, 3, 4, 6, 1, 2]
-    let start = 0
-    let end = array.length - 1
-    let mid = Math.floor((start+end)/2)
+    //let array = [5, 3, 4, 6, 1, 2]
+    
     function buildTreeTest(arr) {
         arr.sort((a, b) => a - b)
+        let start = 0
+        let end = arr.length - 1
+        let mid = Math.floor((start+end)/2)
         let root = arr[mid]
         return root
     }
-    expect(buildTreeTest(array)).toEqual(3)
+    expect(buildTreeTest([5, 3, 4, 6, 1, 2])).toEqual(3)
+})
+
+test('Test tree', () => {
+    class Tree {
+        constructor(arr){
+            this.root = buildTree(arr)
+        }
+    }
+    let testTree = new Tree([1, 2, 3, 4])
+    expect(testTree.root).toEqual(2)
+})
+
+test('Remove duplicates', () => {
+    function removeDuplicates(arr) {
+        //let arr = [1, 1, 2, 3, 3, 5, 8, 8]
+        let newArr = [...new Set(arr)]
+        return newArr
+    }
+    expect(removeDuplicates([1, 1, 2, 3, 3, 5, 8, 8])).toEqual([1, 2, 3, 5, 8])
 })
