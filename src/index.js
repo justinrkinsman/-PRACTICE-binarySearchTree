494ed677f1206f5503e01349e04429d836be5a33
Justin <justinrkinsman@gmail.com>
2022-08-15 10:57:11 -0300

    Add tests for insert.js

--- src/index.js
+++ src/index.js
@@ -55,7 +55,21 @@ function buildTree(arr) {
 }
 
 function sortedArrayToBST(arr, start, end){
-    
+    if (start > end){
+        return null
+    }
+    arr.sort((a, b) => a - b)
+    let array = removeDuplicates(arr)
+    let mid = Math.floor((start + end)/2)
+    let node = new Node(array[mid])
+    node.left = sortedArrayToBST(array, start, mid - 1);
+    node.right = sortedArrayToBST(array, mid + 1, end)
+    return node
 }
+
+//let tree = new Tree([100, 200, 300])
+console.log(buildTree([2, 4, 1, 6, 7]))
+let useThisArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+console.log(sortedArrayToBST(useThisArr, 0, (useThisArr.length - 1)))
 
 //let root = null
